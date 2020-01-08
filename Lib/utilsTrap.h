04678f33e4698e1b6c92fea90b387d4231eb8293
Sergey.Maslennikov <Sergey.Maslennikov@winpharma.com>
2020-01-08 16:29:34 +0300

    Add: Trap

---
+++ Lib/utilsTrap.h
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// utilsTrap.h
+//
+// Standard ISO/IEC 114882, C++14
+//
+// |   version  |    release    | Description
+// |------------|---------------|---------------------------------
+// |      1     |   2017 03 28  |
+// |      2     |   2017 04 28  | Corrected tMessageTrap::Try(...)
+// |      3     |   2020 01 08  | Refactored
+// |            |               |
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <libConfig.h>
+
+#include <cstddef>
+
+#include <vector>
+
+namespace utils
+{
+	namespace trap
+	{
+
+template <const char * const Data, int DataSize, bool DataReverse = false>
+class tTrap
+{
+	int m_DataIndex = 0;
+
+public:
+	bool Try(std::vector<char>& data)
+	{
+		for (std::size_t i = 0; i < data.size(); ++i)
+		{
+			if (DataReverse)
+			{
+				if (Data[DataSize - 1 - m_DataIndex++] == data[i])
+				{
+					if (m_DataIndex == DataSize)
+					{
+						m_DataIndex = 0;
+
+						return true;
+					}
+				}
+				else if (Data[DataSize - 1] == data[i])
+				{
+					m_DataIndex = 1;
+				}
+				else
+				{
+					m_DataIndex = 0;
+				}
+			}
+			else
+			{
+				if (Data[m_DataIndex++] == data[i])
+				{
+					if (m_DataIndex == DataSize)
+					{
+						m_DataIndex = 0;
+
+						return true;
+					}
+				}
+				else if (Data[0] == data[i])
+				{
+					m_DataIndex = 1;
+				}
+				else
+				{
+					m_DataIndex = 0;
+				}
+			}
+		}
+
+		return false;
+	}
+};
+
+	}
+}
