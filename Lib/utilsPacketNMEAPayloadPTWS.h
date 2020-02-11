02b22effd18039d02a01c0d41ffa2eea52ad33d9
Sergey.Maslennikov <Sergey.Maslennikov@winpharma.com>
2020-02-11 13:05:40 +0300

    Add: PTWS

---
+++ Lib/utilsPacketNMEAPayloadPTWS.h
@@ -0,0 +1,145 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// utilsPacketNMEAPayloadPTWS.h
+//
+// Standard ISO/IEC 114882, C++11
+//
+// |   version  |    release    | Description
+// |------------|---------------|---------------------------------
+// |      1     |   2020 02 11  |
+// |            |               | 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "utilsBase.h"
+#include "utilsPacketNMEA.h"
+#include "utilsPacketNMEAType.h"
+
+namespace utils
+{
+	namespace packet_NMEA
+	{
+///////////////////////////////////////////////////////////////////////////////////////////////////
+struct tPayloadPTWS_JAM_SIGNAL_VAL
+{
+	const char* PayloadID = "PTWS";
+	const char* PayloadMsgID = "JAM";
+	const char* PayloadMsgID2 = "SIGNAL";
+	const char* PayloadMsgCMD = "VAL";
+	const char* PayloadMsgVAL1 = "INDEX";
+	const char* PayloadMsgVAL2 = "FREQ";
+
+	typedef Type::tUInt<tUInt8, 0> index_type;
+	typedef Type::tFloat<0, 6> frequency_type;
+
+	index_type Index;
+	frequency_type Frequency;
+
+	tPayloadPTWS_JAM_SIGNAL_VAL() = default;
+	explicit tPayloadPTWS_JAM_SIGNAL_VAL(tUInt8 index, double frequency)
+		:Index(index), Frequency(frequency)
+	{
+	}
+	explicit tPayloadPTWS_JAM_SIGNAL_VAL(const tPayloadCommon::value_type& val)
+	{
+
+		if (val.size() == 8 &&
+			!std::strcmp(val[0].c_str(), PayloadID) &&
+			!std::strcmp(val[1].c_str(), PayloadMsgID) &&
+			!std::strcmp(val[2].c_str(), PayloadMsgID2) &&
+			!std::strcmp(val[3].c_str(), PayloadMsgCMD) &&
+			!std::strcmp(val[4].c_str(), PayloadMsgVAL1)&&
+			!std::strcmp(val[6].c_str(), PayloadMsgVAL2))
+		{
+			Index = index_type(val[5]);
+			Frequency = frequency_type(val[7]);
+		}
+	}
+
+	tPayloadCommon::value_type GetPayload() const
+	{
+		tPayloadCommon::value_type Data;
+
+		Data.push_back(PayloadID);
+		Data.push_back(PayloadMsgID);
+		Data.push_back(PayloadMsgID2);
+		Data.push_back(PayloadMsgCMD);
+		Data.push_back(PayloadMsgVAL1);
+		Data.push_back(Index.ToString());
+		Data.push_back(PayloadMsgVAL2);
+		Data.push_back(Frequency.ToString());
+
+		return Data;
+	}
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+struct tPayloadPTWS_VERSION_GET
+{
+	const char* PayloadID = "PTWS";
+	const char* PayloadMsgID = "VERSION";
+	const char* PayloadMsgCMD = "GET";
+
+	tPayloadPTWS_VERSION_GET() = default;
+	explicit tPayloadPTWS_VERSION_GET(const tPayloadCommon::value_type& val)
+	{
+		if (val.size() == 3 &&
+			!std::strcmp(val[0].c_str(), PayloadID) &&
+			!std::strcmp(val[1].c_str(), PayloadMsgID) &&
+			!std::strcmp(val[2].c_str(), PayloadMsgCMD))
+		{
+
+		}
+	}
+
+	tPayloadCommon::value_type GetPayload() const
+	{
+		tPayloadCommon::value_type Data;
+
+		Data.push_back(PayloadID);
+		Data.push_back(PayloadMsgID);
+		Data.push_back(PayloadMsgCMD);
+
+		return Data;
+	}
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+struct tPayloadPTWS_VERSION_VAL
+{
+	const char* PayloadID = "PTWS";
+	const char* PayloadMsgID = "VERSION";
+	const char* PayloadMsgCMD = "VAL";
+
+	typedef std::string version_type;
+
+	version_type Version;
+
+	tPayloadPTWS_VERSION_VAL() = default;
+	explicit tPayloadPTWS_VERSION_VAL(version_type version)
+		:Version(version)
+	{
+	}
+	explicit tPayloadPTWS_VERSION_VAL(const tPayloadCommon::value_type& val)
+	{
+		if (val.size() == 4 &&
+			!std::strcmp(val[0].c_str(), PayloadID) &&
+			!std::strcmp(val[1].c_str(), PayloadMsgID) &&
+			!std::strcmp(val[2].c_str(), PayloadMsgCMD))
+		{
+			Version = val[3];
+		}
+	}
+
+	tPayloadCommon::value_type GetPayload() const
+	{
+		tPayloadCommon::value_type Data;
+
+		Data.push_back(PayloadID);
+		Data.push_back(PayloadMsgID);
+		Data.push_back(PayloadMsgCMD);
+		Data.push_back(Version);
+
+		return Data;
+	}
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+	}
+}
--- Lib/utilsPacketNMEAPayload_Test.cpp
+++ Lib/utilsPacketNMEAPayload_Test.cpp
@@ -1,6 +1,7 @@
 #include "utilsPacketNMEA.h"
 #include "utilsPacketNMEAPayload.h"
 #include "utilsPacketNMEAPayloadPMTK.h"
+#include "utilsPacketNMEAPayloadPTWS.h"
 
 #include "utilsBase.h"
 #include "utilsTest.h"
@@ -11,13 +12,12 @@
 namespace utils
 {
 
-template<class TPayload>
-void UnitTest_PacketNMEAPayload(const std::string& msg)
+template<class TPayload, class TArg>
+void UnitTest_PacketNMEAPayload(const TArg& msg)
 {
 	typedef utils::packet::tPacket<utils::packet_NMEA::tFormatNMEA, utils::packet_NMEA::tPayloadCommon> tPacketNMEA;
 
-	std::string Raw(msg);
-	tVectorUInt8 DataVector(Raw.begin(), Raw.end());
+	tVectorUInt8 DataVector(msg.cbegin(), msg.cend());//C++14
 
 	tPacketNMEA Packet;
 
@@ -39,6 +39,20 @@ void UnitTest_PacketNMEAPayload(const std::string& msg)
 	}
 }
 
+template<class TPayload>
+void UnitTest_PacketNMEAPayload(const TPayload& val)
+{
+	typedef utils::packet::tPacket<utils::packet_NMEA::tFormatNMEA, utils::packet_NMEA::tPayloadCommon> tPacketNMEA;
+
+	utils::packet_NMEA::tPayloadCommon::value_type PacketData = val.GetPayload();
+
+	tPacketNMEA Packet(PacketData);
+
+	tVectorUInt8 RawPacket = Packet.ToVector();
+
+	UnitTest_PacketNMEAPayload<TPayload>(RawPacket);
+}
+
 void UnitTest_PacketNMEAPayload()
 {
 	std::cout << "\n""utils::packet_NMEA::Payload\n";
@@ -55,53 +69,41 @@ void UnitTest_PacketNMEAPayload()
 		std::cout << std::string(RawPacket.cbegin(), RawPacket.cend()) << '\n';//C++14
 	}
 
-	UnitTest_PacketNMEAPayload<tPayloadGGA<15, 3, 4, 4, 5, 2, 3, 2>>("$GNGGA,172905.087,,,,,0,0,,,M,,M,,*51\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGGA<15, 0, 4, 4, 5, 2, 3, 2>>("$GPGGA,134734,0000.0000,N,00000.0000,E,6,02,,00280.70,M,014.50,M,,*52\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGGA<15, 3, 4, 4, 5, 2, 3, 2>, std::string>("$GNGGA,172905.087,,,,,0,0,,,M,,M,,*51\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGGA<15, 0, 4, 4, 5, 2, 3, 2>, std::string>("$GPGGA,134734,0000.0000,N,00000.0000,E,6,02,,00280.70,M,014.50,M,,*52\xd\xa");
 
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,3,1,10,23,38,230,44,29,71,156,47,07,29,116,41,08,09,081,36*7F\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,3,2,10,10,07,189,,05,05,220,,09,34,274,42,18,25,309,44*72\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,3,3,10,26,82,187,47,28,43,056,46*77\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,3,1,10,23,38,230,44,29,71,156,47,07,29,116,41,08,09,081,36*7F\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,3,2,10,10,07,189,,05,05,220,,09,34,274,42,18,25,309,44*72\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,3,3,10,26,82,187,47,28,43,056,46*77\xd\xa");
 
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,4,1,13,28,59,070,15,13,52,205,30,15,49,261,21,17,43,151,*71\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,4,2,13,24,26,290,,19,26,168,16,30,23,113,16,11,19,050,29*70\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,4,3,13,01,15,065,18,18,13,311,10,10,10,342,14,20,03,255,*79\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GPGSV,4,4,13,08,01,029,*49\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,4,1,13,28,59,070,15,13,52,205,30,15,49,261,21,17,43,151,*71\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,4,2,13,24,26,290,,19,26,168,16,30,23,113,16,11,19,050,29*70\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,4,3,13,01,15,065,18,18,13,311,10,10,10,342,14,20,03,255,*79\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GPGSV,4,4,13,08,01,029,*49\xd\xa");
 
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GLGSV,3,1,09,75,74,051,,84,40,051,,85,39,123,,66,37,279,*68\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GLGSV,3,2,09,74,23,089,,67,22,338,,65,12,223,,83,05,008,*62\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadGSV>("$GLGSV,3,3,09,86,03,164,*52\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GLGSV,3,1,09,75,74,051,,84,40,051,,85,39,123,,66,37,279,*68\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GLGSV,3,2,09,74,23,089,,67,22,338,,65,12,223,,83,05,008,*62\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadGSV, std::string>("$GLGSV,3,3,09,86,03,164,*52\xd\xa");
 	
-	UnitTest_PacketNMEAPayload<tPayloadRMC<13, 3, 4, 4>>("$GNRMC,221325.000,V,,,,,0.00,0.00,241016,,,N*56\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadRMC<13, 3, 4, 4>>("$GPRMC,082653.100,A,2446.4768,N,12100.0344,E,0.00,128.42,270705,,,A*67\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadRMC<13, 3, 6, 6>>("$GNRMC,090210.000,A,5539.564975,N,03732.411956,E,0.03,274.40,120517,,,A*71\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadRMC<13, 3, 4, 4>, std::string>("$GNRMC,221325.000,V,,,,,0.00,0.00,241016,,,N*56\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadRMC<13, 3, 4, 4>, std::string>("$GPRMC,082653.100,A,2446.4768,N,12100.0344,E,0.00,128.42,270705,,,A*67\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadRMC<13, 3, 6, 6>, std::string>("$GNRMC,090210.000,A,5539.564975,N,03732.411956,E,0.03,274.40,120517,,,A*71\xd\xa");
 
-	UnitTest_PacketNMEAPayload<tPayloadPMTK001>("$PMTK001,604,3*32\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadPMTK010>("$PMTK010,001*2E\xd\xa");
-	UnitTest_PacketNMEAPayload<tPayloadPMTK011>("$PMTK011,MTKGPS*08\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadPMTK001, std::string>("$PMTK001,604,3*32\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadPMTK010, std::string>("$PMTK010,001*2E\xd\xa");
+	UnitTest_PacketNMEAPayload<tPayloadPMTK011, std::string>("$PMTK011,MTKGPS*08\xd\xa");
 	
-	//DRAFT
-	{
-		tPayloadPMTK314 Val;
+	UnitTest_PacketNMEAPayload(tPayloadPMTK314());
+	UnitTest_PacketNMEAPayload(tPayloadPMTK314(1, 2));
 
-		utils::packet_NMEA::tPayloadCommon::value_type PacketData1 = Val.GetPayload();
+	UnitTest_PacketNMEAPayload(tPayloadPTWS_VERSION_GET());
+	UnitTest_PacketNMEAPayload(tPayloadPTWS_VERSION_VAL());
+	UnitTest_PacketNMEAPayload(tPayloadPTWS_VERSION_VAL("M33-asdfasdf.fff.df"));
 
-		tPacketNMEA Packet2(PacketData1);
+	UnitTest_PacketNMEAPayload(tPayloadPTWS_JAM_SIGNAL_VAL());
+	UnitTest_PacketNMEAPayload(tPayloadPTWS_JAM_SIGNAL_VAL(12, 1575.000002));
+	UnitTest_PacketNMEAPayload<tPayloadPTWS_JAM_SIGNAL_VAL, std::string>("$PTWS,JAM,SIGNAL,VAL,INDEX,8,FREQ,1574.990234*65\xd\xa");
 
-		tVectorUInt8 RawPacket = Packet2.ToVector();
-
-		std::cout << std::string(RawPacket.cbegin(), RawPacket.cend()) << '\n';//C++14
-	}
-	{
-		tPayloadPMTK314 Val(1, 2);
-
-		utils::packet_NMEA::tPayloadCommon::value_type PacketData1 = Val.GetPayload();
-
-		tPacketNMEA Packet2(PacketData1);
-
-		tVectorUInt8 RawPacket = Packet2.ToVector();
-
-		std::cout << std::string(RawPacket.cbegin(), RawPacket.cend()) << '\n';//C++14
-	}
 
 	std::cout << std::endl;
 }
