658be0db852293619879c611c117a69c3a3915e8
Sergey Maslennikov <maslennikovserge@yandex.ru>
2020-08-12 16:58:17 +0300

    Mod: utilsLog

--- LIB.Utils/utilsLog.cpp
+++ LIB.Utils/utilsLog.cpp
@@ -1,8 +1,9 @@
 #include "utilsLog.h"
 
 #include <chrono>
-#include <cstdio>
 #include <ctime>
+#include <iomanip>
+#include <sstream>
 
 #ifdef LIB_UTILS_LOG
 
@@ -25,23 +26,6 @@ void tLog::Write(bool timestamp, tLogColour textColour, const std::string& msg)
 	WriteLog(timestamp, false, textColour, msg);
 }
 
-void tLog::Write(bool timestamp, tLogColour textColour, const char* format, ...)
-{
-	char Buffer[160];
-
-	std::va_list Args;
-
-	va_start(Args, format);
-
-	std::vsprintf(Buffer, format, Args);
-
-	std::string Str(Buffer);
-
-	va_end(Args);
-
-	WriteLog(timestamp, false, textColour, Str);
-}
-
 void tLog::WriteLine()
 {
 	WriteLog(false, true, tLogColour::Default, "");
@@ -52,35 +36,17 @@ void tLog::WriteLine(bool timestamp, tLogColour textColour, const std::string& m
 	WriteLog(timestamp, true, textColour, msg);
 }
 
-void tLog::WriteLine(bool timestamp, tLogColour textColour, const char* format, ...)
-{
-	char Buffer[160];
-
-	std::va_list Args;
-
-	va_start(Args, format);
-
-	std::vsprintf(Buffer, format, Args);
-
-	std::string Str(Buffer);
-
-	va_end(Args);
-
-	WriteLog(timestamp, true, textColour, Str);
-}
-
 void tLog::WriteHex(bool timestamp, tLogColour textColour, const std::string& msg, const tVectorUInt8& data)
 {
-	std::string Str = msg + '\n';
+	std::stringstream Stream;
 
-	char Figure[5];
+	Stream << msg + '\n';
 
 	std::string Substr;
 
 	for (std::size_t i = 0; i < data.size(); ++i)
 	{
-		std::sprintf(Figure, "%02X ", data[i]);
-		Str += Figure;
+		Stream << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(data[i]) << ' ';
 
 		if (data[i] <= 0x20 || data[i] == 0x25)
 		{
@@ -93,109 +59,101 @@ void tLog::WriteHex(bool timestamp, tLogColour textColour, const std::string& ms
 
 		if (((i + 1) % 16) == 0)
 		{
-			Str += "  " + Substr;
+			Stream << "  " + Substr;
 
 			Substr.clear();
 
 			if (i < data.size() - 1)//It's not needed for the last string
 			{
-				Str += '\n';
+				Stream << '\n';
 			}
 		}
 		else if (((i + 1) % 8) == 0)
 		{
-			Str += "  ";
+			Stream << "  ";
 		}
 	}
 
-	std::size_t FullStringsEnd = Str.find_last_of('\n');
+	const std::size_t Remains = data.size() % 16;
 
-	std::size_t Filled = Str.size() - FullStringsEnd;
-
-	const std::size_t StrHexPratLineSize = 53;
-
-	if (Filled <= StrHexPratLineSize)
+	if (Remains)
 	{
-		std::size_t Empty = StrHexPratLineSize - Filled;
+		std::size_t Empty = (16 - Remains) * 3 + (Remains > 8 ? 2 : 4);
 
 		for (std::size_t i = 0; i < Empty; ++i)
 		{
-			Str += ' ';
+			Stream << ' ';
 		}
 
-		Str += Substr;
+		Stream << Substr;
 	}
 
-	WriteLog(timestamp, true, textColour, Str);
+	WriteLog(timestamp, true, textColour, Stream.str().c_str());
 }
 
 void tLog::WriteLog(bool timestamp, bool endl, tLogColour textColour, const std::string& msg)
 {
 	std::lock_guard<std::mutex> Lock(m_Mtx);
 
+	std::stringstream Stream;
+
 	if (timestamp)
 	{
-		auto TimeNow = std::chrono::high_resolution_clock::now();
+		const auto TimeNow = std::chrono::high_resolution_clock::now();
 
-		std::chrono::duration<unsigned long long, std::micro> Time_us = std::chrono::duration_cast<std::chrono::microseconds>(TimeNow.time_since_epoch());
+		const std::chrono::duration<unsigned long long, std::micro> Time_us = std::chrono::duration_cast<std::chrono::microseconds>(TimeNow.time_since_epoch());
 
-		std::chrono::duration<unsigned long> Time_s = std::chrono::duration_cast<std::chrono::seconds>(TimeNow.time_since_epoch());
+		const std::chrono::duration<unsigned long> Time_s = std::chrono::duration_cast<std::chrono::seconds>(TimeNow.time_since_epoch());
 
-		unsigned long Tm_us = static_cast<decltype(Tm_us)>(Time_us.count() % 1000000);
+		const unsigned long Tm_us = static_cast<decltype(Tm_us)>(Time_us.count() % 1000000);
 
-		char Str[20];
-
-		std::sprintf(Str, "[%.5ld.%.6ld]", Time_s.count(), Tm_us);
-
-		WriteLog(Str);
+		Stream << '[' << std::setfill(' ') << std::setw(10) << Time_s.count() << '.' << std::setw(6) << Tm_us << ']';
 	}
 
 	if (m_ColourEnabled)
 	{
-		std::string Str = "\x1b[";
+		Stream << "\x1b[";
 
 		switch (textColour)
 		{
-		case tLogColour::Black: Str += "30"; break;
-		case tLogColour::Red: Str += "31"; break;
-		case tLogColour::Green: Str += "32"; break;
-		case tLogColour::Yellow: Str += "33"; break;
-		case tLogColour::Blue: Str += "34"; break;
-		case tLogColour::Magenta: Str += "35"; break;
-		case tLogColour::Cyan: Str += "36"; break;
-		case tLogColour::White: Str += "37"; break;
-		case tLogColour::Default: Str += "39"; break;
-		case tLogColour::LightGray: Str += "90"; break;
-		case tLogColour::LightRed: Str += "91"; break;
-		case tLogColour::LightGreen: Str += "92"; break;
-		case tLogColour::LightYellow: Str += "93"; break;
-		case tLogColour::LightBlue: Str += "94"; break;
-		case tLogColour::LightMagenta: Str += "95"; break;
-		case tLogColour::LightCyan: Str += "96"; break;
-		case tLogColour::LightWhite: Str += "97"; break;
-		default: Str += "39"; break;
+		case tLogColour::Black: Stream << "30"; break;
+		case tLogColour::Red: Stream << "31"; break;
+		case tLogColour::Green: Stream << "32"; break;
+		case tLogColour::Yellow: Stream << "33"; break;
+		case tLogColour::Blue: Stream << "34"; break;
+		case tLogColour::Magenta: Stream << "35"; break;
+		case tLogColour::Cyan: Stream << "36"; break;
+		case tLogColour::White: Stream << "37"; break;
+		case tLogColour::Default: Stream << "39"; break;
+		case tLogColour::LightGray: Stream << "90"; break;
+		case tLogColour::LightRed: Stream << "91"; break;
+		case tLogColour::LightGreen: Stream << "92"; break;
+		case tLogColour::LightYellow: Stream << "93"; break;
+		case tLogColour::LightBlue: Stream << "94"; break;
+		case tLogColour::LightMagenta: Stream << "95"; break;
+		case tLogColour::LightCyan: Stream << "96"; break;
+		case tLogColour::LightWhite: Stream << "97"; break;
+		default: Stream << "39"; break;
 		}
 
-		Str += "m" + msg + "\x1b[0m";
+		Stream << "m" + msg + "\x1b[0m";
 
 		if (endl)
 		{
-			Str += '\n';
-		}
-
-		WriteLog(Str);
+			Stream << '\n';
+		}	
 	}
 	else
 	{
+		Stream << msg;
+
 		if (endl)
 		{
-			WriteLog(msg + '\n');
-		}
-		else
-		{
-			WriteLog(msg);
+			Stream << '\n';
 		}
 	}
+
+	WriteLog(Stream.str().c_str());
 }
 
 }
--- LIB.Utils/utilsLog.h
+++ LIB.Utils/utilsLog.h
@@ -5,28 +5,7 @@
 //
 // |   version  |    release    | Description
 // |------------|---------------|---------------------------------
-// |      1     |    long ago   |
-// |      2     |   2016 05 16  | std::string instead of ssystem::String
-// |      3     |   2016 05 30  | Added WriteHex(std::vector<char> msg, bool timestamp = true)
-// |            |               | Added WriteHex(std::string msg, std::vector<char>& data, bool timestamp)
-// |      4     |   2016 06 03  | Added WriteLog(std::string msg, bool timestamp, bool error);
-// |            |               | Added void WriteInfoBlock(std::string msg)
-// |            |               | Corrected WriteHex(std::string msg, std::vector<char>& data, bool timestamp)
-// |      5     |   2016 06 29  | Slightly modified output
-// |      6     |   2016 07 06  | Added virtual void WriteLineArg(bool timestamp, const char* format, ...)
-// |      7     |   2016 07 27  | Corrected void tLog::Write(std::string msg, bool timestamp)
-// |      8     |   2016 09 16  | Added virtual void WriteInfoLine(std::string msg, bool timestamp = true);
-// |      9     |   2016 11 15  | Corrected void tLog::WriteHex(std::string msg, std::vector<char>& data, bool timestamp)
-// |     10     |   2017 03 06  | Added LIB_UTILS_LOG
-// |     11     |   2017 03 29  | Introduced namespace utils::log
-// |     12     |   2017 04 07  | Introduced LIB_UTILS_LOG_ENDL
-// |     13     |   2017 10 29  | Added WriteInfoLineArg(bool timestamp, std::string msg)
-// |     14     |   2018 02 10  | Added support of colours
-// |     15     |   2018 09 27  | Added Write(std::string msg, tLogColour textColour, bool timestamp = true)
-// |            |               | Added WriteLine(std::string msg, tLogColour textColour, bool timestamp = true)
-// |     16     |   2019 04 25  | Refactored
-// |     17     |   2019 05 11  | Updated
-// |     18     |   2020 01 17  | Refactored, thread safe
+// |      1     |    long ago   | ... before 2016 05 16
 // |            |               |
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma once
@@ -35,7 +14,6 @@
 
 #include <utilsBase.h>
 
-#include <cstdarg>
 #include <string>
 #include <mutex>
 
@@ -79,11 +57,9 @@ public:
 	void ColourEnabled(bool state);
 
 	void Write(bool timestamp, tLogColour textColour, const std::string& msg);
-	void Write(bool timestamp, tLogColour textColour, const char* format, ...);
 
 	void WriteLine();
 	void WriteLine(bool timestamp, tLogColour textColour, const std::string& msg);
-	void WriteLine(bool timestamp, tLogColour textColour, const char* format, ...);
 
 	void WriteHex(bool timestamp, tLogColour textColour, const std::string& msg, const tVectorUInt8& data);
 
@@ -106,11 +82,9 @@ public:
 	void ColourEnabled(bool state) { }
 
 	void Write(bool timestamp, tLogColour textColour, const std::string& msg) { }
-	void Write(bool timestamp, tLogColour textColour, const char* format, ...) { }
 
 	void WriteLine() { }
 	void WriteLine(bool timestamp, tLogColour textColour, const std::string& msg) { }
-	void WriteLine(bool timestamp, tLogColour textColour, const char* format, ...) { }
 
 	void WriteHex(bool timestamp, tLogColour textColour, const std::string& msg, const tVectorUInt8& data) { }
 
